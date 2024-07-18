d4d091e63424fd09dd05c977c0fe258eaf041e5d
DilanMaigua <dilanmaigua@hotmail.com>
2024-07-18 10:29:43 -0500

    Implementación del consumo de archivos .json para trabajar los escenarios outline

--- src/test/java/com/everis/base/stepDefinitions/DemoBlazeSD.java
+++ src/test/java/com/everis/base/stepDefinitions/DemoBlazeSD.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import net.thucydides.core.annotations.Step;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import net.thucydides.core.annotations.Steps;
+import org.jruby.RubyProcess;
 
 public class DemoBlazeSD {
 
@@ -23,11 +25,6 @@ public class DemoBlazeSD {
     public void inicializoUnPostEnSignup() {
         demoblaze.incializoParametrosRequestPost();
     }
-
-   // @And("creo un nuevo usuario ""([^""]*)""")
-   // public void creoUnNuevoUsuarioConUsernameYPassword(String user, String pass) {
-   //     demoblaze.crearUsuario(user,pass);
-   // }
 
     @And("ejecuto la creacion de \"([^\"]*)\"$")
     public void ejecutoLaCreacionDe(String api) {
@@ -70,15 +67,16 @@ public class DemoBlazeSD {
     }
 
 
-    @And("creo un nuevo usuario {string}")
-    public void creoUnNuevoUsuario(String json) {
+    @And("creo un nuevo usuario desde el indice {int} del archivo {string}")
+    public void creoUnNuevoUsuarioDesdeElIndiceDelArchivo(int index, String jsonFile)  {
         try {
+
+            String jsonContent = new String(Files.readAllBytes(Paths.get("src/test/resources/" + jsonFile)));
             ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(json);
-
-            String user = jsonNode.get("user").asText();
-            String pass = jsonNode.get("pass").asText();
-
+            JsonNode ruta = objectMapper.readTree(jsonContent);
+            JsonNode indexJson = ruta.get("examples").get(index);
+            String user = indexJson.get("user").asText();
+            String pass = indexJson.get("pass").asText();
             demoblaze.crearUsuario(user,pass);
 
         } catch (Exception e) {
@@ -86,14 +84,15 @@ public class DemoBlazeSD {
         }
     }
 
-    @And("inicio sesion {string}")
-    public void inicioSesion(String json) {
+    @And("inicio sesion desde el indice {int} del archivo {string}")
+    public void inicioSesionDesdeElIndiceDelArchivo(int index,String json) {
         try {
+            String jsonContent = new String(Files.readAllBytes(Paths.get("src/test/resources/" + json)));
             ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(json);
-
-            String user = jsonNode.get("user").asText();
-            String pass = jsonNode.get("pass").asText();
+            JsonNode ruta = objectMapper.readTree(jsonContent);
+            JsonNode indexJson = ruta.get("examples").get(index);
+            String user = indexJson.get("user").asText();
+            String pass = indexJson.get("pass").asText();
 
             demoblaze.inicarSesion(user,pass);
 
@@ -101,4 +100,6 @@ public class DemoBlazeSD {
             e.printStackTrace();
         }
     }
+
+
 }
